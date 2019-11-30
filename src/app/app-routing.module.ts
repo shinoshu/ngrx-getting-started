91f62b3cd17d788078303e2aa277c34cc64d8b37
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-01 02:12:46 +0900

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './hero/heroes.component';
+import { HeroListComponent } from './hero/hero-list/hero-list.component';
 import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', component: HeroesComponent },
+  { path: 'heroes', component: HeroListComponent },
   { path: 'heroes/:id', component: HeroDetailComponent }
 ];
 
