6cfd1405c1bb295219e7c07902b1ae4ecfe55e21
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-01 01:34:58 +0900

    update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { DefaultDataServiceConfig, EntityDataModule } from '@ngrx/data';
 
+import { DemoMaterialModule } from './demo-material-module';
 import { entityConfig } from './entity-metadata';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './hero/heroes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,11 +18,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     EntityDataModule.forRoot(entityConfig),
-    BrowserAnimationsModule
+    DemoMaterialModule
   ],
   bootstrap: [AppComponent]
 })
